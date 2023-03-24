bb593802dd710186da55b1c35ea65a713d7d2501
Ludawi <95353216+Ludawi@users.noreply.github.com>
2023-03-24 08:29:35 +0100

    fixed error handling and naming

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+set -x
 wget -q --spider http://github.com
 
-set -e
-
-if [[ $? -eq 1 ]]; then
+if [ $? -eq 1 ] 
+then
   echo " [ ERR ] no connection to www.github.com \n breaking..."
   exit 1
 fi
@@ -14,39 +15,41 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y systemd python3 pip git
 
-pip install rpi time
+pip install rpi.gpio python-time
 
 echo " [ OK ] Installation done, downloading script."
 
-git clone www.github.com/Ludawi/nameOfProject
-cd nameOfProject
+git clone www.github.com/Ludawi/fancontroller
+cd fancontroller
 
 echo " [ OK ] Download succesful, installing..."
 
 sed -i "s/qUSER/$USER/" fan.service
 
-mkdir /home/"$USER"/fancontrol
-cp gpio.py /home/"$USER"/fancontrol/
+mkdir /home/"$USER"/fancontroller
+cp gpio.py /home/"$USER"/fancontroller/
 cp fan.service /etc/systemd/system/
 
 systemctl enable fan.service 
 systemctl start fan.service
 
-service = systemctl is-active --quiet fan.service
+service=systemctl is-active -q --quiet fan.service
 
-if [[ $service == 1 ]]; then
+if [ $service == 0 ]
+then # How does this work?
   echo " [ WARNING ] Error while running the service."
   echo " [ WARNING ] Service installed and NOT enabled."
+elif [ $service == 1 ] 
+then
+  echo " [ OK ] Service installed and enabled."
+  echo "Restarting the device is advised. \n Do you want to restart now? (y/N): "
 fi
-
-echo " [ OK ] Service installed and enabled."
-echo "Restarting the device is advised. \n Do you want to restart now? (y/N): "
 
 read decision
 
 if [[ $decision == y ]]; then
   echo "rebooting ..."
-  sleep(5)
+  sleep 5
   reboot
 fi
 
