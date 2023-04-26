8abdc86301ee97a4ada64b10668b3e16cb9249f3
Ludawi <95353216+Ludawi@users.noreply.github.com>
2023-04-26 11:52:13 +0200

    fixed whitespace problem

--- gpio.py
+++ gpio.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
-import time # die GPIOS koennen über die PINNummer oder die GPIONummer
+import time
+# die GPIOS koennen über die PINNummer oder die GPIONummer
 # angesprochen werden
-GPIO.setmode(GPIO.BCM) # hier GPIO-Nummer
-GPIO.setup(23, GPIO.OUT) interval_in_s = 120.0
+GPIO.setmode(GPIO.BCM)# hier GPIO-Nummer
+GPIO.setup(23, GPIO.OUT)
+interval_in_s = 120.0
 while(True):
-    print("on")
-    GPIO.output(23, GPIO.HIGH)
-    time.sleep(interval_in_s) # sleep interval in Sekunden
-    print("off")
-    GPIO.output(23, GPIO.LOW)
-    time.sleep(interval_in_s)
+  print("on")
+  GPIO.output(23, GPIO.HIGH)
+  time.sleep(interval_in_s)# sleep interval in Sekunden
+  print("off")
+  GPIO.output(23, GPIO.LOW)
+  time.sleep(interval_in_s)
